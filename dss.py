03fe44ca5c2c774b75a164cf9a1bbed8e9342c51
chaleyuo <83237942+chaleyuo@users.noreply.github.com>
2021-05-10 08:21:39 -0700

    Update dss.py

--- dss.py
+++ dss.py
@@ -1,4 +1,7 @@
 import pandas as pd
+%%capture
+!pip install pyforest
+import pyforest 
 
 def remove_list_item (*, the_list, the_item):
   new_list = [item for item in the_list if item != the_item]
@@ -12,4 +15,17 @@ def plot_x_by_class_y(*, table, x_column, y_column):
   assert table[y_column].nunique()<=5, f'y_column must be of 5 categories or less but has {table[y_column].nunique()}'
 
   pd.crosstab(table[x_column], table[y_column]).plot(kind='bar', figsize=(15,8), grid=True, logy=True)
+  return None
+
+def percent_change (*, new, old):
+  change = (new - old)/old
+  return change
+
+def replace_outliers (*, df, column):
+  sigma = np.std(df['column'])
+  sigma3 = sigma * 3
+  high_wall = df['column'].mean() + sigma3
+  column_list = df['column'].tolist()
+  cleaned_column = [min(item, high_wall) for item in column_list]
+  df['column']= cleaned_column
   return None
